e81570a67fcb6d235d837c5254f7ee49cc06095b
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-08 18:27:38 +0800

    change url.

--- ansible.alv.pub/ansible/scripts/update.ansible.hosts.py
+++ ansible.alv.pub/ansible/scripts/update.ansible.hosts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 #coding:utf-8
 import os
-url='https://raw.githubusercontent.com/AlvinWanCN/TechnologyCenter/master/sophiroth_cluster/ansible.alv.pub/ansible/conf.d/hosts'
+url='https://raw.githubusercontent.com/AlvinWanCN/sophiroth-cluster/master/ansible.alv.pub/ansible/conf.d/hosts'
 file='/etc/ansible/hosts'
 #serviceName='ansible'
 os.system('curl -fsSL %s > %s'%(url,file))
