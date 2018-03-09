881f9bf7a0bd38834dfabe70a8f92a355b0fa00b
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-09 15:40:03 +0800

    new

--- pxe_system/scripts/resetAnsibleFingerprint.py
+++ pxe_system/scripts/resetAnsibleFingerprint.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python
 #coding:utf-8
-import os,re
-ipstr=os.popen('ip a s ens32|grep global').read() #获取关于ip信息的字符串
-ip=re.findall(r'\w\s(.*)\/',ipstr)[0]  #截取ip最后一位
+import os,re,urllib2,socket
+hostname=socket.gethostname().split('.')[0]
 host='http://ansible.alv.pub'
 path='/cgi-bin/resetHostFingerprint.py'
-querys='user=alvin&host=%s'%ip
+querys='user=alvin&host=%s'%hostname
 url=host + path + '?' + querys
-
-os.system('timeout 3 curl %s'%url)
+urllib2.urlopen(url)
