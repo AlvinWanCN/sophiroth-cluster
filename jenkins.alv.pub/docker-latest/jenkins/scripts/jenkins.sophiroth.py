155fc99f0729858b907c6cf26825b2cba5cf9683
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-09 17:21:04 +0800

    add readme.md

--- jenkins.alv.pub/docker-latest/jenkins/scripts/jenkins.sophiroth.py
+++ jenkins.alv.pub/docker-latest/jenkins/scripts/jenkins.sophiroth.py
@@ -1,19 +1,28 @@
 #!/usr/bin/python
 #coding:utf=8
-import sys
+import sys,os
 
 num=sys.argv[1]
 
 
 def n01():
     #item name: 01-update_ansible.alv.pub_ansible_hosts
-    print('01')
+    os.system('scp /jenkins/workspace/01-update_ansible.alv.pub_ansible_hosts/ansible.alv.pub/ansible/conf.d/hosts ansible:/etc/ansible/hosts')
 
 def n02():
     #item name: 02-update_dc.alv.pub_ldapUserData_alvin_scripts_welcome.py
-    print('02')
+    os.system('cp /jenkins/workspace/02-update_dc.alv.pub_ldapUserData_alvin_scripts_welcome.py/python/sophiroth.welcome.py /sophiroth/alvin/')
+def n03():
+    #item name: 03-update_dc.alv.pub_pxe_default
+    os.system('scp /jenkins//workspace/03-update_dc.alv.pub_pxe_default/pxe_system/conf.d/default dc:/var/lib/tftpboot/pxelinux.cfg/default')
 
-
+def n04():
+    #item name: 04 - updateAndReload_.alv.pub_named
+    pass
+    #this item do not use this script
+def n05():
+    #item name:
+    os.system('scp /jenkins/workspace/05-updateAndRestart_dhcp.alv.pub_dhcpd/dhcp.alv.pub/dhcpd/conf.d/dhcpd.conf dhcp:/etc/dhcp/dhcpd.conf')
 
 
 def main():
@@ -21,6 +30,14 @@ def main():
         n01()
     elif num == '02':
         n02()
+    elif num == '03':
+        n03()
+    elif num == '04':
+        n04()
+    elif num == '05':
+        n05()
+    else:
+        pass
 
 
 if __name__ == '__main__':
