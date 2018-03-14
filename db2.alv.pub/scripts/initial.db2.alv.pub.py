5e65746d1f1e7e9695fa36476edb5e08f903ca7d
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-14 15:30:07 +0800

    update

---
+++ db2.alv.pub/scripts/initial.db2.alv.pub.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+#coding:utf-8
+import os
+
+os.system('curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/TechnologyCenter/master/linux/software/galera.repo > /etc/yum.repos.d/galera.repo') #add mariadb galera cluster repository
+os.system('yum install MariaDB-Galera-server -y') #install mariadb-galera-server
+os.system('curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/sophiroth-cluster/master/db1.alv.pub/galera-mariadb/conf.d/server.cnf > /etc/my.cnf.d/server.cnf')
+os.system('systemctl start mysql ')
