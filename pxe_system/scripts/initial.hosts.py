f2958422df9d065c6aa27f3f7715f8ef282020d8
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-15 10:42:51 +0800

    update

--- pxe_system/scripts/initial.hosts.py
+++ pxe_system/scripts/initial.hosts.py
@@ -5,4 +5,4 @@ hostname=socket.gethostname()
 
 def initital_host(host):
     url='https://raw.githubusercontent.com/AlvinWanCN/sophiroth-cluster/master/%s/scripts/initial.%s.py'%(host,host)
-    os.system('curl -fsSL ')
+    os.system('curl -fsSL %s'%url)
