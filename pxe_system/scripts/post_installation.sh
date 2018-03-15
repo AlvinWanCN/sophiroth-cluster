0f086a25ab87a58812cb567f8a16cbc5f2a55b84
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-15 10:53:23 +0800

    changed some content

--- pxe_system/scripts/post_installation.sh
+++ pxe_system/scripts/post_installation.sh
@@ -6,6 +6,6 @@ python -c "$(curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/scripts/mas
 python -c "$(curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/scripts/master/common_tools/joinNatashaLDAP.py)" #加入到我的ldap
 sed -i 's/%wheel.*/%wheel  ALL=(ALL) NOPASSWD:  ALL/' /etc/sudoers #设置wheel组nopasswd，便于ansible的操作。
 grep /sophiroth/alvin/scripts/welcome.py /etc/profile || echo "/sophiroth/alvin/scripts/welcome.py" >> /etc/profile # 添加登录时执行的脚本。脚本网络地址：https://github.com/AlvinWanCN/scripts/raw/master/python/sophiroth.welcome.py
-echo "0 */2 * * * /usr/sbin/ntpdate alv.pub" >> /var/spool/cron/root
+echo "0 */2 * * * /usr/sbin/ntpdate alv.pub >/dev/null" >> /var/spool/cron/root
 python -c "$(curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/sophiroth-cluster/master/pxe_system/scripts/resetAnsibleFingerprint.py)" #访问ansible api,修改本服务器的指纹。
 python -c "$(curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/sophiroth-cluster/master/$(hostname)/scripts/initial.$(hostname).py)" #每台服务器执行自己相应的配置脚本
