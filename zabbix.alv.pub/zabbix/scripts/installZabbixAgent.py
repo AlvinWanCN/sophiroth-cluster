8d16bc26fab8442cb911cd15ce3a1fdcf28fe17a
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-16 14:25:36 +0800

    update

---
+++ zabbix.alv.pub/zabbix/scripts/installZabbixAgent.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+import os
+
+os.system('curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/TechnologyCenter/master/linux/software/yum.repos.d/zabbix3.4.repo > /etc/yum.repos.d/zabbix3.4.repo')
+os.system('yum install zabbix-agent -y')
+os.system('sed -i "s/^Hostname.*/Hostname=$(hostname)/" /etc/zabbix/zabbix_agentd.conf')
+os.system('sed -i "s/^Server.*/Server=zabbix.alv.pub/" /etc/zabbix/zabbix_agentd.conf')
+os.system('systemctl start zabbix-agent')
+os.system('systemctl enable zabbix-agent')
