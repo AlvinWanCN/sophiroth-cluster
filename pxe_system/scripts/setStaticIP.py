2162c3df36cd130f0298ae1d782943189fea1492
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-28 15:16:26 +0800

    update

---
+++ pxe_system/scripts/setStaticIP.py
@@ -0,0 +1,25 @@
+#!usr/bin/python
+#_*_coding:utf-8_*_
+import subprocess
+
+ip=subprocess.check_output('ip a s|grep global|cut -d/ -f1|cut -dt  -f2|cut -d" " -f2',shell=True).split('\n')[0]
+netFile='/etc/sysconfig/network-scripts/ifcfg-ens32'
+
+#将dhcp改成static
+subprocess.call("sed -i 's/BOOTPROTO=.*/BOOTPROTO=static/' %s "%netFile,shell=True)
+
+ipdict={}
+ipdict['NETMASK']='255.255.255.0'
+ipdict['GATEWAY']='192.168.127.254'
+ipdict['DNS1']='47.75.0.56'
+ipdict['DNS2']='114.114.114.114'
+ipdict['DOMAIN']='alv.pub shenmin.com'
+
+def changeIP(key):
+    if subprocess.call('grep %s %s'%(key,netFile),shell=True) == 0:
+        subprocess.call("sed -i 's/%s=.*/%s=%s/' %s"%(key,key,ipdict[key],netFile),shell=True)
+    else:
+        subprocess.call('echo "%s=%s > %s"'%(key,ipdict[key],netFile),shell=True)
+
+for i in ipdict.keys():
+    changeIP(i)
