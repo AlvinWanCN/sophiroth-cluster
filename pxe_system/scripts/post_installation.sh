156ddd6f877e187f0be02e985a3b34553e6b3b77
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-03-28 17:48:22 +0800

    添加了静态IP的设置。

--- pxe_system/scripts/post_installation.sh
+++ pxe_system/scripts/post_installation.sh
@@ -3,6 +3,7 @@ python -c "$(curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/sophiroth-c
 python -c "$(curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/sophiroth-cluster/master/pxe_system/scripts/getAnsibleSSHKey.py)" #添加ansible主机ssh public key.
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/scripts/master/common_tools/disableSeAndFir.sh)" #自定义脚本的方式关闭防火墙
 python -c "$(curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/scripts/master/common_tools/pullLocalYum.py)" #添加本地yum仓库
+bash -c "$(curl -fsSL https://github.com/AlvinWanCN/scripts/raw/master/common_tools/sshslowly.sh)" ##关闭SSH的UseDNS
 python -c "$(curl -fsSL https://raw.githubusercontent.com/AlvinWanCN/scripts/master/common_tools/joinNatashaLDAP.py)" #加入到我的ldap
 sed -i 's/%wheel.*/%wheel  ALL=(ALL) NOPASSWD:  ALL/' /etc/sudoers #设置wheel组nopasswd，便于ansible的操作。
 grep /sophiroth/alvin/scripts/welcome.py /etc/profile || echo "/sophiroth/alvin/scripts/welcome.py" >> /etc/profile # 添加登录时执行的脚本。脚本网络地址：https://github.com/AlvinWanCN/scripts/raw/master/python/sophiroth.welcome.py
