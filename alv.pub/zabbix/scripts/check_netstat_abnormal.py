5b7266e13c2dc893f42d44bbd98c8809173cea78
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-05-11 17:22:37 +0800

    update

--- alv.pub/zabbix/scripts/check_netstat_abnormal.py
+++ alv.pub/zabbix/scripts/check_netstat_abnormal.py
@@ -5,7 +5,7 @@ import os
 ##检查是否有我们自己设定之外的网络连接出现，没有则返回0，有则返回出现的内容。
 
 
-basicCommand='netstat -anplut|grep -ivE "connections|Address|10050|10051|801|mysqld|sshd|docker-proxy|pptp|ntpd|slapd|named|wechat|140.207|8003|:53|389|203.205.128|git-remote|183.136.203.17:80|127.0.0.1:4006|nginx|:22122|:23000|172.31.33.93:443"'
+basicCommand='netstat -anplut|grep -ivE "connections|Address|10050|10051|801|mysqld|sshd|docker-proxy|pptp|ntpd|slapd|named|wechat|140.207|8003|:53|389|203.205.128|git-remote|183.136.203.17:80|127.0.0.1:4006|nginx|:22122|:23000|172.31.33.93:443|172.31.33.93:80"'
 
 ## 183.136.203.17:80 is get weather.
 ## 203.205.128 is wechat api address
