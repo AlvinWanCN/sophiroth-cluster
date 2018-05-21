13c68bfbb2b6bc14e3292d9a661640b3da76c009
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-05-21 15:00:16 +0800

    update

--- alv.pub/named/scripts/update.named.alv.pub.py
+++ alv.pub/named/scripts/update.named.alv.pub.py
@@ -2,7 +2,7 @@
 #coding:utf-8
 import os
 url='https://raw.githubusercontent.com/AlvinWanCN/sophiroth-cluster/master/alv.pub/named/conf.d/alv.pub.zone'
-file='/var/named/alv.pub.zone '
+file='/var/named/alv.pub.zone'
 serviceName='named'
 os.system('curl -fsSL %s > %s'%(url,file))
 os.system('systemctl reload %s'%serviceName)
