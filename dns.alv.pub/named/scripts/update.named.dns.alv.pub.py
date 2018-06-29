430012a8d7e0df7246b429510eecc5d1edf9a873
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-06-29 10:33:51 +0800

    update

--- dns.alv.pub/named/scripts/update.named.dns.alv.pub.py
+++ dns.alv.pub/named/scripts/update.named.dns.alv.pub.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 #coding:utf-8
-import os
+import os,time
 url='https://raw.githubusercontent.com/AlvinWanCN/sophiroth-cluster/master/dns.alv.pub/named/conf.d/alv.pub.zone'
 file='/var/named/alv.pub.zone'
 serviceName='named'
 os.system('curl -fsSL %s > %s'%(url,file))
+time.sleep(3)
 os.system('systemctl reload %s'%serviceName)
