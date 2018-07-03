40a4db8de580d93a56c94e39e1d108ccd1697088
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-07-03 15:02:14 +0800

    update

--- saltstack.alv.pub/scripts/startup_sophirothpxe.py
+++ saltstack.alv.pub/scripts/startup_sophirothpxe.py
@@ -1,5 +1,13 @@
 #coding:utf-8
 import subprocess,os
-dir='/opt/SophirothPXE'
-os.chdir(dir)
-subprocess.call('ls',shell=True)
+
+maindir='/opt/'
+project='SophirothPXE'
+workdirk=maindir+project
+port=8001
+logdir='/var/log/sophirothpxe/'
+logfile=logdir+'sophiroth.log'
+
+os.chdir(workdirk)
+subprocess.call('mkdir -p %s'%logdir,shell=True)
+subprocess.call('nohup /usr/bin/python3 -m http.server --cgi %s & >>%s &'%(port,logfile),shell=True)
