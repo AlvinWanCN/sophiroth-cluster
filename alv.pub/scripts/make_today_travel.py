7b15b12d151b518d03d6909f96ab8be4bf813bc7
Alvin Wan <alvin.wan.cn@hotmail.com>
2018-08-09 16:42:41 +0800

    update

--- alv.pub/scripts/make_today_travel.py
+++ alv.pub/scripts/make_today_travel.py
@@ -7,9 +7,14 @@ os.chdir(WORK_DIR)
 
 TODAY_DIR=WORK_DIR+time.strftime('%Y/%B')+'/'
 TODAY_DIR_FILE=TODAY_DIR+time.strftime('%d.%b.%Y.%a')+'.rst'
+print('Start Pull ')
 subprocess.call('git pull',shell=True)
 subprocess.call('mkdir -p %s'%TODAY_DIR,shell=True)
+print('Start cp readme to new')
 subprocess.call('cp readme.rst %s'%TODAY_DIR_FILE,shell=True)
+print('Start add file to git')
 subprocess.call('git add %s'%TODAY_DIR_FILE,shell=True)
+print('Start commit ')
 subprocess.call('git commit -m "add new daily" %s'%TODAY_DIR_FILE,shell=True)
-subprocess.call('git push origin master')
+print('Start push')
+subprocess.call('git push origin master',shell=True)
